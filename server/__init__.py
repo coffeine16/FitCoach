b9168c4fe07c2ae5b03be869b292c4151bb6d997
coffeine16 <keshav.agrawalcaff@gmail.com>
2026-04-24 03:54:26 +0530

    Grand Finale

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Fitcoach environment server components."""
+
+from .FitCoach_environment import FitcoachEnvironment
+
+__all__ = ["FitcoachEnvironment"]
